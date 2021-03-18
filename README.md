91074fc04d1a504e84ee55e5ef1aaa0f1e63f217
mstolovich <mstolovich@gmail.com>
2021-03-18 14:51:48 +0200

    CU-q107r1 Adding Payment Modal

--- README.md
+++ README.md
@@ -1,24 +1,48 @@
-# stripe-payment-modal-vue-example
+# Stripe Payment Modal Vue Demo
 
-## Project setup
-```
-yarn install
-```
+This sample project shows how to implement Stripe payments & subscriptions within a modal dialog using [PaymentModal](https://paymentmodal.com) and [Vue CLI](https://cli.vuejs.org).
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+### Demo
 
-### Compiles and minifies for production
-```
-yarn build
-```
+See a hosted version of the Next JS sample project [here](https://vuejs.paymentmodal.com).
 
-### Lints and fixes files
-```
-yarn lint
-```
+The hosted demo is running in Stripe test mode. Use `4242424242424242` as a test card number with any CVC + future expiration date.
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Getting Started
+
+This is a [Vue.js](https://vuejs.org/) project bootstrapped with [Vue CLI](https://cli.vuejs.org/).
+
+## Installation
+
+To see the Stripe modals in action, you need to create a free [PaymentModal account](https://paymentmodal.com).
+
+1. Create a [PaymentModal site](https://paymentmodal.com).
+2. Clone this repo
+3. In the project directory, run `cp .env.local.sample .env.local`
+4. Open `.env.local` and update `VUE_APP_PAYMENT_MODAL_SITE_ID` with your [PaymentModal site id](https://paymentmodal.com).
+5. Run `yarn` to download npm packages.
+6. Run `yarn start` to open the Vue app in development mode.
+7. Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
+8. Update the `Demo.vue` in `src/components` to use any HTML element attributes to attach click listeners to open your modals.
+
+This sample and hosted demo project uses the following HTML element attributes, but you can use any valid [HTML selector](https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Selectors#reference_table_of_selectors). Be sure to update the selectors in your PaymentModal account.
+
+- `id="pm-payment-amount"` creates a modal with a Stripe payment intent using an amount.
+- `class="pm-payment-stripeid"` creates a modal with a Stripe payment intent using a Stripe price id.
+- `data-subscription-checkout="true"` creates a modal with a Stripe subscription using a Stripe price id.
+
+## Learn More
+
+You can learn more about configuring Stripe modal properties in the [PaymentModal documentation](https://paymentmodal.com/docs).
+
+You can learn more about the Vue app in the [Vue.js](https://vuejs.org/) and [Vue CLI](https://cli.vuejs.org/).
+
+#### Get Support
+
+If you found a bug or want to suggest a new [feature/use case/sample], please [file an issue](https://github.com/funnelkake/stripe-payment-modal-vue-example/issues).
+
+If you have questions, comments, or need help with the code, we're here to help:
+
+on Twitter at [@paymentmodal](https://twitter.com/paymentmodal)
+on Stack Overflow at the stripe-modal tag
+by [email](mailto:support@paymentmodal.com?subject=[GitHub]%20Source%Vue%20Demo)
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -7,10 +7,27 @@
           <div class="card-header">Amount Charge</div>
           <div class="card-body">
             <h5 class="card-title">$10</h5>
-            <p class="card-text">
-              Amount set in Admin Panel
-            </p>
-            <button type="button" class="btn btn-primary">
+            <div class="card-text">
+              <p>
+                Amount set in Admin Panel
+              </p>
+              <label for="payment-amount-quantity"> Quantity </label>
+              <br />
+              <select
+                id="payment-amount-quantity"
+                v-on:change="changeItem('#pm-payment-amount', $event)"
+                class="browser-default custom-select"
+              >
+                <option selected value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <button
+              type="button"
+              id="pm-payment-amount"
+              class="btn btn-primary"
+            >
               Amount Charge
             </button>
           </div>
@@ -21,10 +38,23 @@
           <div class="card-header">Stripe Price Charge</div>
           <div class="card-body">
             <h5 class="card-title">$39</h5>
-            <p class="card-text">
-              Payment with Stripe Price Id
-            </p>
-            <button type="button" class="btn btn-primary">
+            <div class="card-text">
+              <p>
+                Payment with Stripe Price Id
+              </p>
+              <label for="payment-stripe-id-quantity"> Quantity </label>
+              <br />
+              <select
+                id="payment-stripe-id-quantity"
+                v-on:change="changeItem('.pm-payment-stripeid', $event)"
+                class="browser-default custom-select"
+              >
+                <option selected value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <button type="button" class="btn btn-primary pm-payment-stripeid">
               Stripe Price Charge
             </button>
           </div>
@@ -35,10 +65,29 @@
           <div class="card-header">Subscription</div>
           <div class="card-body">
             <h5 class="card-title">$49<small>/mo</small></h5>
-            <p class="card-text">
-              Subscription Charge
-            </p>
-            <button type="button" class="btn btn-primary">
+            <div class="card-text">
+              <p>
+                Subscription Charge
+              </p>
+              <label for="subscription-quantity"> Quantity </label>
+              <br />
+              <select
+                id="subscription-quantity"
+                v-on:change="
+                  changeItem('[data-subscription-checkout=true]', $event)
+                "
+                class="browser-default custom-select"
+              >
+                <option selected value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-subscription-checkout="true"
+            >
               Subscribe
             </button>
           </div>
@@ -56,8 +105,28 @@ export default {
   components: {
     MDBContainer,
   },
+  mounted() {
+    let externalScript = document.createElement("script");
+    externalScript.setAttribute(
+      "src",
+      `${process.env.VUE_APP_PAYMENT_MODAL_API_HOST_URL}/api/stripe_modals/script.js?siteId=${process.env.VUE_APP_PAYMENT_MODAL_SITE_ID}`
+    );
+    externalScript.setAttribute("defer", "defer");
+    document.body.appendChild(externalScript);
+  },
   props: {
     msg: String,
+  },
+  methods: {
+    changeItem: (openModalSelector, event) => {
+      const openModalElement = document.querySelector(
+        `button${openModalSelector}`
+      );
+      openModalElement.setAttribute(
+        "data-product-quantity",
+        event.target.value
+      );
+    },
   },
 };
 </script>
@@ -77,6 +146,7 @@ h1 {
 }
 .card {
   border-radius: 8px;
+  box-shadow: lightgray 0px 1px;
 }
 .card-title {
   padding-top: 20px;
